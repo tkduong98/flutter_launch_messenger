245163401d476af4f1eff7b4e0080fefa5ab859f
tkduong98 <55155107+tkduong98@users.noreply.github.com>
2024-01-02 22:14:35 +0700

    Update SwiftFlutterLaunchPlugin.swift

--- ios/Classes/SwiftFlutterLaunchPlugin.swift
+++ ios/Classes/SwiftFlutterLaunchPlugin.swift
@@ -3,21 +3,19 @@ import UIKit
 
 public class SwiftFlutterLaunchPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "br.com.thyagoluciano/flutter_launch", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "vn.com.cotoiday/flutter_launch", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterLaunchPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
 
-    if ("launchWhatsapp" == call.method) {
+    if ("launchMessenger" == call.method) {
       let args = call.arguments as! Dictionary<String, String>
-      let phone = args["phone"]
-      let message = args["message"]
+        let idUser = args["id_user"]
+        let urlMessenger = "fb-messenger-public://user-thread/\(idUser)"
 
-      let urlString = "whatsapp://send?phone=\(phone ?? "0")&message=\(message ?? "0")"
-
-      let urlStringEncoded = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+      let urlStringEncoded = urlMessenger.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
       let URL = NSURL(string: urlStringEncoded!)
 
       if UIApplication.shared.canOpenURL(URL! as URL) {
@@ -30,8 +28,8 @@ public class SwiftFlutterLaunchPlugin: NSObject, FlutterPlugin {
       let name = args["name"]
 
        switch name ?? "0" {
-         case "whatsapp":
-           result(schemeAvailable(scheme: "whatsapp://send"))
+         case "messenger":
+           result(schemeAvailable(scheme: "fb-messenger-public://user-thread/"))
            break
          default:
              result(false)
